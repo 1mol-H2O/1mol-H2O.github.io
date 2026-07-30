1f49ae615c1e7e71ab75e6f6d3a15a1869584a94
1mol氢氧酸 <1mol-H2O@users.noreply.github.com>
2026-07-31 00:28:05 +0800

    20260731

--- resource/image/background/background.js
+++ resource/image/background/background.js
@@ -1,3 +1,3 @@
-const imageFiles = ['1.png', '2.jpg', '3.webp', '4.webp', '5.webp', '6.png', '7.jpg', '8.png', '9.jpeg'];
-const num = Math.floor(Math.random() * imageFiles.length);
-document.body.style.backgroundImage = `url('resource/image/background/${imageFiles[num]}')`;
+const FileType=Math.random()<0.5?'jpeg':'webp';
+const num = Math.floor(Math.random() * 8) + 1;
+document.body.style.backgroundImage = `url('resource/image/background/.${FileType}/${num}.${FileType}')`;
