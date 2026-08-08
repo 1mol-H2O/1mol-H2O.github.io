e9b02b8cc84d704c9a6d6a8d92664f14345130b1
1mol氢氧酸 <1mol-H2O@users.noreply.github.com>
2026-08-09 01:05:01 +0800

    背景图没改好，再改一次

--- js/background.js
+++ js/background.js
@@ -1,3 +1,3 @@
 const FileType = Math.random() < 0.5 ? 'avif' : 'webp';
 const num = Math.floor(Math.random() * 8) + 1;
-document.body.style.backgroundImage = `url('../resource/image/background/.${FileType}/${num}.${FileType}')`;
+document.body.style.backgroundImage = `url('resource/image/background/${FileType}/${num}.${FileType}')`;
